e2d275a6e10c83f0f7ab588815a9e4d9140cad34
khoi <khoiracle@gmail.com>
2018-11-29 01:37:02 +0700

    add propTypes

--- src/containers/MenuBar.js
+++ src/containers/MenuBar.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import Header from "../components/Header";
 import { updateFile } from "../actions";
-
+import PropTypes from "prop-types";
 import React, { Component } from "react";
 
 class MenuBar extends Component {
@@ -29,6 +29,12 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   onFileNameChange: (id, name) => dispatch(updateFile(id, name))
 });
+
+MenuBar.propTypes = {
+  id: PropTypes.string.isRequired,
+  fileName: PropTypes.string.isRequired,
+  onFileNameChange: PropTypes.func.isRequired,
+};
 
 export default connect(
   mapStateToProps,
