5ad625c488a1f3ae6d2fde2aaee13dd73582e8fe
khoi <khoiracle@gmail.com>
2018-11-28 20:53:22 +0700

    Save state to local storage

--- src/index.js
+++ src/index.js
@@ -7,8 +7,17 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import { createStore } from "redux";
 import rootReducer from "./reducers";
 import { Provider } from "react-redux";
+import { loadState, saveState } from "./storages/localStorage";
+import throttle from "lodash/throttle";
 
-const store = createStore(rootReducer);
+const persistedState = loadState();
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 
 ReactDOM.render(
   <Provider store={store}>
