990ed8b16fc3eb478e474dba8dd9fe99be1d2d1d
khoi <khoiracle@gmail.com>
2018-11-28 14:16:58 +0700

    Simple editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import SimpleMDEEditor from "react-simplemde-editor";
+import "simplemde/dist/simplemde.min.css";
 
 class App extends Component {
+  handleChange = value => {
+    this.setState({ mdeValue: value });
+  };
+
   render() {
     return (
-      <div className="App">
-          Main App Component
+      <div>
+        <SimpleMDEEditor
+          onChange={this.handleChange}
+          options={{
+            autofocus: true
+          }}
+        />
       </div>
     );
   }
