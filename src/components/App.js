9fc14717f7982956f91211946f4d0533474bc227
khoi <khoiracle@gmail.com>
2018-11-29 02:21:59 +0700

    FileTree component placeholder

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "simplemde/dist/simplemde.min.css";
 import MenuBar from "../containers/MenuBar";
 import Editor from "../containers/Editor";
+import FileTree from "../containers/FileTree";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <>
         <MenuBar />
         <Editor />
+        <FileTree />
       </>
     );
   }
---
+++ src/containers/FileTree.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FileTree extends Component {
+  render() {
+    return <div>This is the file tree</div>;
+  }
+}
+
+export default FileTree;
