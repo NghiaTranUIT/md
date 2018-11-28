3ca7dadd78c1b2b35b41de308817bbf02cad729c
khoi <khoiracle@gmail.com>
2018-11-28 21:15:18 +0700

    redux dev tool

--- src/index.js
+++ src/index.js
@@ -11,7 +11,11 @@ import { loadState, saveState } from "./storages/localStorage";
 import throttle from "lodash/throttle";
 
 const persistedState = loadState();
-const store = createStore(rootReducer, persistedState);
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 store.subscribe(
   throttle(() => {
